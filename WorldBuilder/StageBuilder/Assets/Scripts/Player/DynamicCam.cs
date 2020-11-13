08e8fbd36f6f10280813cdce94cf90c2b63669ae
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2020-11-13 16:25:06 -0500

    stuff

---
+++ WorldBuilder/StageBuilder/Assets/Scripts/Player/DynamicCam.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicCam : MonoBehaviour
+{
+    public GameObject target;
+    public float distance = 3f;
+    Vector3 MoveDirection;
+    public float speed = 10f;
+
+    void Awake()
+    {
+        gameObject.transform.SetParent(null);
+    }
+
+   
+    void FixedUpdate()
+    {       
+        if (Vector3.Distance(target.gameObject.transform.position, transform.position) > distance)
+        {
+            Vector3 dir = transform.forward;
+            dir.y = 0;
+
+            Vector3 location = gameObject.transform.position;
+
+            location += (speed * Time.fixedDeltaTime * dir);
+            gameObject.transform.position = location;
+        }
+    }
+    void Update()
+    {
+        MoveDirection = (target.gameObject.transform.position - transform.position).normalized;
+        gameObject.transform.forward = MoveDirection;
+    }
+}
